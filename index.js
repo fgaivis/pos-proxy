6b0888d3a5349c93d32ed420959a12c9bf5cce54
Francisco Gaivis <francisco@gaivis.com>
2016-06-19 16:39:53 -0500

    Apuntando al puerto 4040

--- index.js
+++ index.js
@@ -4,13 +4,3 @@ var http = require('http'),
 // Create your proxy server and set the target in the options.
 //
 httpProxy.createProxyServer({target:'http://prime.mqa-labs.com:8080/mqalabs-integration-pos-facade-1.0/api-rest'}).listen(4040);
-
-//
-// Create your target server
-//
-/*http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
-  res.end();
-}).listen(9000);*/
-
