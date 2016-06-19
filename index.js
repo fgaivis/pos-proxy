d731fbe2bf2c6eec561a5cfb4e4df8e7e97c29ac
Francisco Gaivis <francisco@gaivis.com>
2016-06-19 17:43:01 -0500

    Configurando el Proxy para aceptar Origin

--- index.js
+++ index.js
@@ -3,4 +3,23 @@ var http = require('http'),
 //
 // Create your proxy server and set the target in the options.
 //
-httpProxy.createProxyServer({target:'http://prime.mqa-labs.com:8080/mqalabs-integration-pos-facade-1.0/api-rest'}).listen(4040);
+//httpProxy.createProxyServer({target:'http://prime.mqa-labs.com:8080/mqalabs-integration-pos-facade-1.0/api-rest'}).listen(4040);
+
+var proxy = httpProxy.createProxyServer({target:'http://prime.mqa-labs.com:8080/mqalabs-integration-pos-facade-1.0/api-rest'});
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+});
+proxy.listen(4040);
+
+//
+// Modifying Headers to the RESPONSE.
+//
+/*
+var proxy = httpProxy.createProxyServer({target:'http://prime.mqa-labs.com:8080/mqalabs-integration-pos-facade-1.0/api-rest'});
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, DELETE, OPTIONS, CONNECT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+});
+proxy.listen(4040);
+*/
